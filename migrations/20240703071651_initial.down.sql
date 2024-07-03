754e35cffe7566691c49bede101231272bb0128a
U-LAPTOP-M6SF6634\hkarc <xkyrchokov@gmail.com>
2024-07-03 10:53:19 +0300

    changes

---
+++ migrations/20240703071651_initial.down.sql
@@ -0,0 +1,14 @@
+-- reverse: create index "idx_wallet_user_id" to table: "wallets"
+DROP INDEX "public"."idx_wallet_user_id";
+-- reverse: create "wallets" table
+DROP TABLE "public"."wallets";
+-- reverse: create "wallet_projections" table
+DROP TABLE "public"."wallet_projections";
+-- reverse: create index "idx_wallet_events_wallet_id_transfer_id_event_type" to table: "wallet_events"
+DROP INDEX "public"."idx_wallet_events_wallet_id_transfer_id_event_type";
+-- reverse: create "wallet_events" table
+DROP TABLE "public"."wallet_events";
+-- reverse: create index "idx_outbox_messages_status_publisher_type" to table: "outbox_messages"
+DROP INDEX "public"."idx_outbox_messages_status_publisher_type";
+-- reverse: create "outbox_messages" table
+DROP TABLE "public"."outbox_messages";
---
+++ migrations/20240703071651_initial.up.sql
@@ -0,0 +1,51 @@
+-- create "outbox_messages" table
+CREATE TABLE "public"."outbox_messages" (
+  "id" uuid NOT NULL,
+  "payload" jsonb NOT NULL,
+  "publisher_type" text NOT NULL,
+  "publisher_options" jsonb NOT NULL,
+  "status" text NOT NULL,
+  "created_at" timestamp NOT NULL,
+  "updated_at" timestamp NOT NULL,
+  PRIMARY KEY ("id")
+);
+-- create index "idx_outbox_messages_status_publisher_type" to table: "outbox_messages"
+CREATE INDEX "idx_outbox_messages_status_publisher_type" ON "public"."outbox_messages" ("status", "publisher_type");
+-- create "wallet_events" table
+CREATE TABLE "public"."wallet_events" (
+  "id" uuid NOT NULL,
+  "version" bigint NOT NULL,
+  "transfer_id" text NOT NULL,
+  "reference_id" text NOT NULL,
+  "wallet_id" uuid NOT NULL,
+  "amount" numeric NOT NULL DEFAULT 0,
+  "event_type" text NOT NULL,
+  "transfer_status" text NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT statement_timestamp(),
+  PRIMARY KEY ("id"),
+  CONSTRAINT "wallet_events_amount_check" CHECK (amount >= (0)::numeric)
+);
+-- create index "idx_wallet_events_wallet_id_transfer_id_event_type" to table: "wallet_events"
+CREATE UNIQUE INDEX "idx_wallet_events_wallet_id_transfer_id_event_type" ON "public"."wallet_events" ("wallet_id", "transfer_id", "event_type");
+-- create "wallet_projections" table
+CREATE TABLE "public"."wallet_projections" (
+  "wallet_id" uuid NOT NULL,
+  "balance" numeric NOT NULL,
+  "pending_debit" numeric NOT NULL,
+  "pending_credit" numeric NOT NULL,
+  "last_event_id" uuid NOT NULL,
+  "created_at" timestamp NULL DEFAULT statement_timestamp(),
+  "updated_at" timestamp NULL DEFAULT statement_timestamp(),
+  PRIMARY KEY ("wallet_id")
+);
+-- create "wallets" table
+CREATE TABLE "public"."wallets" (
+  "id" uuid NOT NULL,
+  "reference_id" text NOT NULL,
+  "created_at" timestamp NULL DEFAULT statement_timestamp(),
+  "updated_at" timestamp NULL DEFAULT statement_timestamp(),
+  PRIMARY KEY ("id"),
+  CONSTRAINT "wallets_reference_id_key" UNIQUE ("reference_id")
+);
+-- create index "idx_wallet_user_id" to table: "wallets"
+CREATE INDEX "idx_wallet_user_id" ON "public"."wallets" ("reference_id");
